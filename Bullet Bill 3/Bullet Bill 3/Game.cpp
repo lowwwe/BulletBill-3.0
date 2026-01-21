15baef7d9a11f1db3d2088c866791cda21a9b667
Peter Lowe <peter.lowe@itcarlow.ie>
2026-01-21 17:22:06 +0000

    zero grtavity

--- Bullet Bill 3/Bullet Bill 3/Game.cpp
+++ Bullet Bill 3/Bullet Bill 3/Game.cpp
@@ -356,6 +356,15 @@ void Game::adjustGravity(float t_adjustment)
 	float magnitude = 0.0f;
 	m_gravity.y += t_adjustment;
 
+	m_gravityBar.setFillColor(sf::Color::Blue);
+	m_arrowSprite.setColor(sf::Color::White);
+	if (m_gravity.y < 0.0f)
+	{
+		m_gravity.y = 0.0f;
+		m_gravityBar.setFillColor(sf::Color::Red);
+		m_arrowSprite.setColor(sf::Color::Blue);
+	}
+
 	magnitude = m_gravity.y * 500.0f + 35.0f;
 	m_gravityBar.setSize(sf::Vector2f{ 20.0f, magnitude });
 	m_arrowSprite.setScale(sf::Vector2f{ 1.0f, magnitude / 60.0f });
